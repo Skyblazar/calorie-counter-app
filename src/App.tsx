9b4e4bfe3cbbf82689492ef492c02ed941469e02
King Elisha <ek.chibueze@gmail.com>
2020-08-06 13:20:36 +0100

    chore: updated contributing.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <h1>App</h1>
     </div>
   );
-}
-
-export default App;
+};
