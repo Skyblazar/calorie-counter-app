9649847ad0bd3650cfce0382762c9c1f58b2d6b4
King Elisha <ek.chibueze@gmail.com>
2020-08-06 15:17:58 +0100

    feat(atoms): added input

---
+++ src/components/atoms/input/index.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  padding: 1em 1.3em;
+  width: 80vw;
+  max-width: 500px;
+  font-weight: bold;
+  border: 0.1px solid rgba(0, 0, 0, 0.05);
+  outline: none;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  margin-top: 7px;
+  background-color: #fff;
+  transition: all 0.3s ease 0s;
+  border-radius: 100px;
+
+  &:hover,
+  &:focus {
+    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.2);
+    transform: translateY(-7px);
+  }
+
+  ::placeholder {
+    color: #ccc;
+  }
+`;
