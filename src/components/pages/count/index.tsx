ec58a646f17b984892164954057c8b36bb5b4abd
King Elisha <ek.chibueze@gmail.com>
2020-08-08 16:41:39 +0100

    feat: redirect to home

--- src/components/pages/count/index.tsx
+++ src/components/pages/count/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import { CalorieBox, FoodList } from "../../organisms";
 import { useStore } from "../../../store";
@@ -6,7 +7,10 @@ import { getPercentages } from "./utils";
 import { PageTemplate } from "../../templates";
 
 export const CountPage = () => {
+  const history = useHistory();
   const { addedFoods, totalCalories } = useStore();
+
+  if (!addedFoods || addedFoods.length === 0) history.replace("/");
 
   return (
     <PageTemplate>
